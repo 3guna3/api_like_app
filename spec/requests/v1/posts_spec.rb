b745e8168b57aa0dac9928e49ccc32b580090c43
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-11-26 19:05:56 +0900

    posts_spec.rbにindexのテストを記述

--- spec/requests/v1/posts_spec.rb
+++ spec/requests/v1/posts_spec.rb
@@ -1,10 +1,34 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "V1::Posts", type: :request do
+  let(:current_user) { create(:user) }
+  let(:headers) { current_user.create_new_auth_token }
+  let(:user) { create(:user) }
+
   describe "GET /index" do
-    it "returns http success" do
-      get "/v1/posts/index"
-      expect(response).to have_http_status(:success)
+    subject { get(v1_post_path, headers: headers) }
+
+    context "トークン認証がない場合" do
+      subject { get(v1_post_path) }
+      let!(:post) { create(:post, user_id: current_user.id) }
+      it "エラーが発生する" do
+        subject
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    before { create_list(:post, 3, user_id: current_user.id) }
+    context "ユーザーの投稿が存在する時" do
+      it "投稿一覧を取得できること" do
+        subject
+        json = JSON.parse(response.body)
+        expect(response).to have_http_status(:ok)
+        expect(json.size).to eq 3
+        expect(json[0].keys).to eq %w[id title content]
+        expect(json[0]["id"]).to eq Post.first.id
+        expect(json[0]["title"]).to eq Post.first.title
+        expect(json[0]["content"]).to eq Post.first.content
+      end
     end
   end
 
@@ -35,5 +59,4 @@ RSpec.describe "V1::Posts", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
