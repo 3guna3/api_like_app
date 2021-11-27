18246805c66a0e6db0938174235b4ae3f99aad04
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-11-27 19:07:56 +0900

    showのrspecを実装

--- spec/requests/v1/posts_spec.rb
+++ spec/requests/v1/posts_spec.rb
@@ -33,9 +33,33 @@ RSpec.describe "V1::Posts", type: :request do
   end
 
   describe "GET /show" do
-    it "returns http success" do
-      get "/v1/posts/show"
-      expect(response).to have_http_status(:success)
+    subject { get(v1_post_path(post_id), headers: headers) }
+    let(:post) { create(:post, user_id: current_user.id) }
+    let(:post_id) { post.id }
+
+    context "トークン認証情報がない場合" do
+      subject { get(v1_post_path(post_id)) }
+      it "エラーが発生する" do
+        subject
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context "指定したidの投稿が存在する場合" do
+      it "指定したidの投稿を取得できること" do
+        subject
+        json = JSON.parse(response.body)
+        expect(response).to have_http_status(:ok)
+        expect(json["title"]).to eq post.title
+        expect(json["content"]).to eq post.content
+      end
+    end
+
+    context "指定したidの投稿が存在しない場合" do
+      let(:post_id) { 0 }
+      it "エラーが発生する" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 
